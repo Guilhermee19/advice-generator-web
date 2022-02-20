2635013dedcdc4ed17737dac4613779e31937640
Guilhermee19 <guilherme.santana1998@gmail.com>
2022-02-20 16:42:43 -0300

    build do project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { DadoIcon, LineWebIcon, LineMobileIcon } from './components/Icons'
+import { ButtonDado } from './components/Buttons/Buttons'
+import api from './service/app';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+   const [tip, setTip] = useState({});
+   const [loading, setLoading] = useState(true);
+
+   async function newTip(){
+      setLoading(true)
+      try{
+         const response = await api.get()
+         setTip(response.data.slip)
+      }  
+      catch{
+         console.log('erro')
+      }    
+      // setTimeout(() => {
+         setLoading(false)
+      // }, 500);
+   } 
+   
+   useEffect(() => {
+      console.log('new')
+      screenTip()
+   }, [loading])
+
+
+   setTimeout(() => {
+      if(loading && Object.keys(tip).length === 0){
+         newTip()
+      }
+   }, 1000);
+
+   const screenTip = () =>{
+      return (
+         <div className="container">
+            <div className='card'>
+
+               {!loading && (
+               <>
+                     <h1 className="title"> 
+                        Advice 
+                        <span id="number_tip"> # {tip.id} </span>
+                     </h1>
+
+                     <div className="text_tip" id="tip"> "{tip.advice}" </div>
+               </>
+               )}
+
+               {loading && (
+               <>
+                     <h1 className="title"> 
+                        Advice 
+                        <span id="number_tip"> # {tip.id} </span>
+                     </h1>
+
+                     <div className='animation_loading'>
+                        <DadoIcon />
+                     </div>
+               </>
+               )}
+
+               <div className="container_tips">
+                  <LineWebIcon className="line line_web" />
+                  <LineMobileIcon className="line line_mobile" />
+
+                  <ButtonDado newTip={newTip} />
+               </div>
+            </div>
+         </div>
+      )
+   }
+
+   return (
+      screenTip()
+   );
 }
 
 export default App;
